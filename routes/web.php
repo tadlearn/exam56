835653c711d2e6fb8fcda7b67f0f02504c041076
吳老師 <33886056+tadlearn@users.noreply.github.com>
2018-08-07 14:00:53 +0800

    修改路由改用控制器

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/exam/create', function () {
-    return view('exam.create');
-})->name('exam.create');
+Route::get('/exam', 'ExamController@index')->name('exam.create');
+Route::get('/exam/create', 'ExamController@create')->name('exam.create');
