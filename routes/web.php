dd0e5b8a355803b877c35daed6ef7eb066b5fbe9
吳老師 <33886056+tadlearn@users.noreply.github.com>
2018-08-08 11:47:40 +0800

    儲存題目

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::get('/exam', 'ExamController@index')->name('exam.index');
 Route::get('/exam/{exam}', 'ExamController@show')->name('exam.show');
 Route::get('/exam/create', 'ExamController@create')->name('exam.create');
 Route::post('/exam', 'ExamController@store')->name('exam.store');
+
+Route::post('/topic', 'TopicController@store')->name('topic.store');
